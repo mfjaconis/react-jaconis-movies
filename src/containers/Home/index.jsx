3980415a0cd7172abf2372c93f42d6e367e51665
mfjaconis <matheusjaconis@gmail.com>
2024-06-10 20:19:56 -0300

    adicionando imagem home de forma dinamica, pegando os dados da api

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -1,18 +1,45 @@
+import { useState, useEffect } from 'react'
+
 import api from '../../services/api'
+import { Background, Info, Poster, Container } from './styles'
 
 function Home() {
-  async function getMovies() {
-    const data = await api.get('/movie/popular')
+  const [movie, setMovie] = useState()
 
-    console.log(data)
-  }
+  useEffect(() => {
+    async function getMovies() {
+      const {
+        data: { results }
+      } = await api.get('/movie/popular')
 
-  getMovies()
+      setMovie(results[0])
+      console.log(results)
+    }
+
+    getMovies()
+  }, [])
 
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <>
+      {movie && (
+        <Background
+          img={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
+        >
+          <Container>
+            <Info>
+              <h1>{movie.title}</h1>
+              <p>{movie.overview}</p>
+            </Info>
+            <Poster>
+              <img
+                src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
+                alt="capa do filme"
+              />
+            </Poster>
+          </Container>
+        </Background>
+      )}
+    </>
   )
 }
 
---
+++ src/containers/Home/styles.js
@@ -0,0 +1,58 @@
+import styled from 'styled-components'
+
+export const Background = styled.header`
+  background-image: url(${(props) => props.img});
+  height: 100vh;
+  background-position: center;
+  background-size: cover;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  &::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+  }
+`
+
+export const Info = styled.div`
+  z-index: 2;
+  padding: 20px;
+  width: 50%;
+
+  h1 {
+    font-size: 5em;
+    font-weight: 700;
+    color: #fff;
+  }
+
+  p {
+    font-size: 20px;
+    font-weight: 500;
+    color: #fff;
+    margin-top: 30px;
+    margin-bottom: 20px;
+  }
+`
+export const Poster = styled.div`
+  z-index: 3;
+
+  img {
+    width: 400px;
+    border-radius: 30px;
+  }
+`
+
+export const Container = styled.section`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  width: 1500px;
+  height: 100%;
+`
